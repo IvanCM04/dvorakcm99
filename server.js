c7b3da9ac8a6c33a0998f5180c842ec30d9943dc
IvanCM04 <55461052+IvanCM04@users.noreply.github.com>
2019-11-21 15:40:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
     if (typeof jugador.nombre === 'string' && 
         typeof jugador.pos === 'object' &&
         typeof jugador.pos.length === 'number' &&
-        jugador.pos.length === '2') {
+        jugador.pos.length === 2) {
         res.end('Correcto');
     } else {
       res.end('Incorrecto');
