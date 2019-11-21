c4b8cf59bf05fec4c928bd5f62e3671e8cd44ade
IvanCM04 <55461052+IvanCM04@users.noreply.github.com>
2019-11-21 15:02:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 http.createServer(function (req, res) {
-  var q = url.parse.(req.url, true).query;  
+  var q = url.parse(req.url, true).query;
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Saludos usuario' + q.nombre);
+  res.end('Saludos usuario ' + q.nombre);
 }).listen(process.env.PORT);
