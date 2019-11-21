83d0b91527d71ad94827097082626bce9d11ddd4
IvanCM04 <55461052+IvanCM04@users.noreply.github.com>
2019-11-21 15:06:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var url = require('url');
+var nombres = [];
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true).query;
+  nombres.push(q.nombre);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Saludos usuario ' + q.nombre);
+  res.end('Saludos ' + nombres.join());
 }).listen(process.env.PORT);
